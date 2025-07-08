330b8ad88aa13da14ea445310178c820eaaeba3a
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2025-07-08 11:14:04 -0600

    doc: se agregagorn puntos debug para ver si esta cargando de forma correcta el archivo

--- lib/services/csv_loader.dart
+++ lib/services/csv_loader.dart
@@ -5,18 +5,26 @@ import '../models/cac_model.dart';
 
 class CsvLoader {
   static Future<List<CAC>> loadCACs() async {
-    final rawData = await rootBundle.loadString('assets/cacs_limpios.csv');
-    final List<List<dynamic>> csvData = const CsvToListConverter().convert(rawData, eol: '\n');
-    
-    // Quitar encabezado
-    final data = csvData.skip(1).map((row) => CAC.fromList(row)).toList();
-    return data;
+    try {
+      print('🔄 Intentando cargar el CSV...');
+      final rawData = await rootBundle.loadString('assets/doc/cacs_limpios.csv');
+      print('✅ CSV cargado con éxito');
+      final List<List<dynamic>> csvData = const CsvToListConverter().convert(rawData, eol: '\n');
+      final data = csvData.skip(1).map((row) => CAC.fromList(row)).toList();
+      print('📦 Total de filas: ${data.length}');
+      return data;
+    } catch (e) {
+      print('❌ Error al cargar CSV: $e');
+      return [];
+    }
   }
 
   static Future<List<String>> getUniqueProducts() async {
     final cacs = await loadCACs();
     final products = cacs.map((e) => e.nombreProducto).toSet().toList();
     products.sort();
+    print('🟢 Productos únicos encontrados: ${products.length}');
     return products;
   }
 }
+
